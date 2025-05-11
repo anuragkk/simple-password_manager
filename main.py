e62fe0ce848da94a7e0dbc1b2d7b0f3d97a6c3dd
=Anurag <=anuragsnd@gmail.com>
2025-05-11 23:24:47 +0530

    final commit

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
     random_number = randint(8, 10)
-    password_letters = [choice(letters) for letter in range(random_number)]
+    password_letters = [choice(letters) for _ in range(random_number)]
     random_number_2 = randint(2, 4)
-    password_symbol = [choice(numbers) for number in range(random_number_2)]
-    password_numbers = [choice(symbols) for symbol in range(random_number_2)]
+    password_symbol = [choice(numbers) for _ in range(random_number_2)]
+    password_numbers = [choice(symbols) for _ in range(random_number_2)]
     password_list = password_letters + password_numbers + password_symbol
     shuffle(password_list)
     password = "".join(password_list)
@@ -31,18 +31,56 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
+    website = website_entry.get().lower()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        with open("data.txt", mode="a") as file:
-            file.write(f"{website} | {email} | {password}\n")
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        data = {}
+
+    data.update(new_data)
+
+    with open("data.json", "w") as data_file:
+        json.dump(data, data_file, indent=4)
+
     website_entry.delete(0, "end")
 
     password_entry.delete(0, "end")
-    messagebox.showinfo(title="success", message="your password is saved")
+
+
+def search():
+    website = website_entry.get()
+
+    if len(website) == 0:
+        return  # Exit if no website is entered
+
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+
+        return  # Exit if file not found
+    except json.JSONDecodeError:
+
+        return  # Exit if the JSON is invalid
+
+    # Check if the website is in the data
+    if website in data:
+        password = data[website]["password"]
+        print(f"Password for {website} is {password}")
+    else:
+        print(f"No details found for {website}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -73,11 +111,12 @@ generate_password_button.grid(row=3, column=2)
 
 """add_button"""
 add_button = Button(text="Add", width=36, command=save)
-
 add_button.grid(row=4, column=1, columnspan=2)
 
+search_button = Button(text="search", command=search)
+search_button.grid(row=1, column=2)
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.insert(0, "anuragsnd@gmail.com")
